bb4ee40b885244d4850b48c242fd5b1487509a2c
anantlk <anant.lk.1997@gmail.com>
2018-01-02 14:46:58 +0530

    update

--- utilities/store_details.py
+++ utilities/store_details.py
@@ -17,22 +17,28 @@ def store(regno, mobile, email, branch):
         "branch": branch
     }
     if(re.match("16[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2016 batch students
-        det = db.stud_det_16
-        result = det.insert_one(details)
-    if(re.match("17[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2017 batch students
-        det = db.stud_det_17
-        result = det.insert_one(details)
+        if (db.stud_det_16.find({"regno":regno}).count()==0):
+			det = db.stud_det_16
+			result = det.insert_one(details)
 
+			
+    if(re.match("17[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2017 batch students
+        if(db.stud_det_17.find({"regno":regno}).count()==0):
+			det = db.stud_det_17
+			result = det.insert_one(details)
+			
 # storing details of the schedule of the student in the database
 
 
 def store_time_table(timetable, regno):
     time_table = db.table
-    schd = collections.OrderedDict()
-    for day in days:
-        i = days.index(day)
-        schd["regno"]=regno
-        schd["day_routine"]=collections.OrderedDict()
-        for pos_day_schedule in range(len(timetable[i])):
-            schd["day_routine"][str(pos_day_schedule + 1)] = str(timetable[i][pos_day_schedule])
-        time_table.update_one({"day": day}, {"$push": {"schedule": schd}})
+    if(time_table.find({"schedule.regno":regno}).count()==0):
+		schd = collections.OrderedDict()
+		for day in days:
+			i = days.index(day)
+			schd["regno"]=regno
+			schd["day_routine"]=collections.OrderedDict()
+			for pos_day_schedule in range(len(timetable[i])):
+				schd["day_routine"][str(pos_day_schedule + 1)] = str(timetable[i][pos_day_schedule])
+			time_table.update_one({"day": day}, {"$push": {"schedule": schd}})
+
