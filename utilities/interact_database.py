a31e8c5cc05c3fccb07465aeae7813793b698b82
Presto412 <priyanshjain412@gmail.com>
2018-01-12 12:48:40 +0530

    First Commit

--- utilities/interact_database.py
+++ utilities/interact_database.py
@@ -2,15 +2,25 @@ from pymongo import MongoClient
 import sys
 import os
 
-sys.path.insert(0,os.path.join(os.getcwd(),'models'))
-client = MongoClient('mongodb://localhost:27017')
-db = client.StudentDetails
+sys.path.insert(0, os.path.join(os.getcwd(), 'models'))
+# client = MongoClient('mongodb://localhost:27017')
+
+DB_NAME = "student_details"
+DB_HOST = "ds249737.mlab.com"
+DB_PORT = 49737
+DB_USER = "ieeecs"
+DB_PASS = "ieeecs@vit"
+connection = MongoClient(DB_HOST, DB_PORT)
+db = connection[DB_NAME]
+db.authenticate(DB_USER, DB_PASS)
+
 
 def create_collection(model):
-	collection=db.table
-	if(collection.count()==0):
-		collection.insert_many(model)
-		
+    collection = db.table
+    if(collection.count() == 0):
+        collection.insert_many(model)
+
+
 def check_database(regno, batch):
     if(batch == "16"):
         if(db.stud_det_16.find({"regno": regno}).count() == 0):
--- utilities/store_details.py
+++ utilities/store_details.py
@@ -4,7 +4,7 @@ import interact_database
 import sys
 import os
 
-sys.path.insert(0,os.path.join(os.getcwd(),'models'));
+sys.path.insert(0, os.path.join(os.getcwd(), 'models'))
 
 import schema
 days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
@@ -13,24 +13,25 @@ days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 
 
 def store(regno, name, email, mobile):
-	details=schema.user(regno,name,email,mobile)
-	if(re.match("16[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2016 batch students
-		if (interact_database.check_database(regno,"16")):
-			interact_database.insert(details,"16")
-	if(re.match("17[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2017 batch students
-		if (interact_database.check_database(regno,"17")):
-			interact_database.insert(details,"17")
-			
+    details = schema.user(regno, name, email, mobile)
+    if(re.match("16[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2016 batch students
+        if interact_database.check_database(regno, "16"):
+            interact_database.insert(details, "16")
+    if(re.match("17[a-zA-Z]{3}[0-9]{4}", regno)):  # for 2017 batch students
+        if interact_database.check_database(regno, "17"):
+            interact_database.insert(details, "17")
+
 # storing details of the schedule of the student in the database
 
 
 def store_time_table(timetable, regno):
-	schema.create_time_table_schema(days)
-	schd = collections.OrderedDict()
-	for day in days:
-		i = days.index(day)
-		schd["regno"]=regno
-		schd["day_routine"]=collections.OrderedDict()
-		for pos_day_schedule in range(len(timetable[i])):
-			schd["day_routine"][str(pos_day_schedule + 1)] = str(timetable[i][pos_day_schedule])
-		interact_database.update(day,schd)
+    schema.create_time_table_schema(days)
+    schd = collections.OrderedDict()
+    for day in days:
+        i = days.index(day)
+        schd["regno"] = regno
+        schd["day_routine"] = collections.OrderedDict()
+        for pos_day_schedule in range(len(timetable[i])):
+            schd["day_routine"][str(pos_day_schedule + 1)] = str(
+                timetable[i][pos_day_schedule])
+        interact_database.update(day, schd)
